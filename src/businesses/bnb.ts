d4b79af12672f23660e0b51e4c3548cf9aaf9896
Leilanigreer <leilani@lpcgolf.com>
2025-03-28 15:24:15 -0700

    Update B&B business information

--- src/businesses/bnb.ts
+++ src/businesses/bnb.ts
@@ -4,7 +4,7 @@ const bnb: Business = {
   id: "bnb",
   name: "B&B",
   tagline: "B&B is ready to wear menswear.",
-  description: "B&B is ready to wear menswear.",
+  description: "B&B. A curated collection of fine menswear clothing - business casual, sportswear, shoes & accessories.",
   logo: "BnBLogo",
   hours: "Open: 12pm-7pm M-F or by appt",
   linkWording: "Visit website",
